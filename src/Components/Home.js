309e28a7df5a05300cca93336f1fb0ec2a2469ab
necka96 <nemanja2014@live.com>
2021-11-01 19:53:10 +0100

    blog post

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React from "react"
+import Typical from 'react-typical'
+import image from "../bg.jpg"
 function Home() {
  return (
-  <div>
-   <h2>Home</h2>
-  </div>
+   <main>
+    <img src={image} alt="mackbook" className="absolute object-cover w-full h-full"/>
+    <section className="relative flex flex-col justify-center items-center min-h-screen  pt-3 py-64 px-12">
+    <h1 className="text-6xl text-blue-100 font-bold sans-serif leading-none lg:leading-snug home-name">Aloha I'm Nemanja Djordjevic </h1>
+    <p className="text-xl text-blue-100">I'm a {''} 
+    <Typical 
+    wrapper = "a"
+    loop={Infinity}
+    steps={["Frond-end developer", 1000, "AOI Viscom operater", 1000, "Economic tehnicial", 1000]}
+    />
+    </p>
+    </section>
+   </main>
  )
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,18 +4,18 @@ import { SocialIcon } from 'react-social-icons';
  function NavBar() {
   return (
      <header className="bg-blue-600">
-   <div className="container mx-auto flex justify-between">
+   <div className="container mx-auto flex justify-between items-center md:items-stretch flex-col md:flex-row">
    <nav className="flex">
    <NavLink to="/" exact 
    activeClassName="text-white"
-   className="inline-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-yellow-300 text-4xl font-bold .sans-serif tracking-widest" >
+   className="inline-flex items-center py-6 px-3 mr-1 md:mr-4 text-blue-100 hover:text-yellow-300 text-lg md:text-4xl font-bold sans-serif tracking-widest" >
     Home
    </NavLink>
     <NavLink to="/post"
     activeClassName="text-red-100 bg-blue-700"
     className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-300"
     >
-     Blog post
+     Post
    </NavLink>
      <NavLink to="/project"
      activeClassName="text-red-100 bg-blue-700"
@@ -30,7 +30,7 @@ import { SocialIcon } from 'react-social-icons';
     About Me!
    </NavLink>
    </nav>
-   <div className="inline-flex py-3 my-6">
+   <div className="inline-flex py-3 my-2 md:my-6">
    <SocialIcon url="https://www.facebook.com/GreenArrowSpectre/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
    <SocialIcon url="https://www.instagram.com/necka96/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
    <SocialIcon url="https://www.youtube.com/channel/UCMhokMd1CaR7kLzGrsgTYxQ" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
